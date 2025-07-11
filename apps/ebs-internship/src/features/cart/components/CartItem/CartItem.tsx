bb2e505da739e0c2899ba0bc7651f24e3ebb698e
Dima <golovichkind@gmail.com>
2025-07-11 16:08:33 +0300

    refactor: use common component

--- apps/ebs-internship/src/features/cart/components/CartItem/CartItem.tsx
+++ apps/ebs-internship/src/features/cart/components/CartItem/CartItem.tsx
@@ -3,12 +3,11 @@ import { useCartItemStyles } from "./CartItemStyles";
 import { useStyles } from "@/styles";
 import type { CartItem as CartItemType } from "@/features/cart/types/cartItem";
 import { Course, IMAGE_FALLBACKS, LOCAL_STORAGE } from "@libs";
-import { Star } from "@/assets";
-import { useTheme } from "antd-style";
 import { Link } from "react-router-dom";
 import { getRouteUrlById, RoutesEnum } from "@/config/routesEnum";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteProduct } from "@/features/cart/api";
+import { CourseStatistics } from "@/components";
 
 interface Props {
   cartItem: CartItemType;
@@ -18,7 +17,6 @@ interface Props {
 export const CartItem = ({ cartItem, course }: Props) => {
   const { styles } = useCartItemStyles();
   const { styles: globalStyles } = useStyles();
-  const theme = useTheme();
 
   const queryClient = useQueryClient();
 
@@ -98,47 +96,16 @@ export const CartItem = ({ cartItem, course }: Props) => {
           gap={8}
         >
           <Flex vertical>
-            <Typography.Title level={5}>{product.name}</Typography.Title>
+            <Typography.Title level={5}>
+              {course?.title ?? product.name}
+            </Typography.Title>
             <Typography.Paragraph className={globalStyles.paragraphSm}>
               By {product.authors[0]?.first_name}{" "}
               {product.authors[0]?.last_name}
             </Typography.Paragraph>
           </Flex>
 
-          <Flex
-            gap={4}
-            align="center"
-            wrap="wrap"
-          >
-            <Typography.Text type="warning">4.6</Typography.Text>
-            <Flex>
-              {Array.from({ length: 5 }, (_, i) => (
-                <Star
-                  key={i}
-                  fill={theme.warning.warning300}
-                  stroke="transparent"
-                  width={20}
-                  height={20}
-                />
-              ))}
-            </Flex>
-            <Typography.Text type="secondary">(250) rating</Typography.Text>
-            {course ? (
-              <>
-                <Typography.Text type="secondary">|</Typography.Text>
-                <Typography.Text>
-                  {course.duration ?? 0} Total Hours.{" "}
-                  {course.lessons?.length ?? 0} Lectures.{" "}
-                  {course.level ?? "Beginner"} level
-                </Typography.Text>
-              </>
-            ) : (
-              <Skeleton.Input
-                size="small"
-                style={{ width: 265 }}
-              />
-            )}
-          </Flex>
+          <CourseStatistics course={course} />
 
           <Flex align="center">
             <Button
